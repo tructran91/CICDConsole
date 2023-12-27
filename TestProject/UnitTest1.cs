59fc68356a8e423672cc43a46870a6d1ddcdd1fa
Truc Tran <trungtruc.tran91@gmail.com>
2023-12-27 15:10:58 +0700

    init

---
+++ TestProject/UnitTest1.cs
@@ -0,0 +1,21 @@
+namespace TestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private const string Expected = "Hello, World!";
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                Program.Main();
+
+                var result = sw.ToString().Trim();
+                Assert.AreEqual(Expected, result);
+            }
+        }
+    }
+}
